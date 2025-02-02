11ca7e9749593269ba7815c0b8048b519f2bdd9a
Aphiwit1 <aphiwit.boon@gmail.com>
2025-02-02 23:15:05 +0700

    add your and vivid collection

--- src/components/MobileSidebar/MobileSidebar.tsx
+++ src/components/MobileSidebar/MobileSidebar.tsx
@@ -12,12 +12,14 @@ interface SidebarLinkProps {
   label: string
   onClick: () => void
   isExternalLink?: boolean
+  className?:string
 }
 
 const SidebarLink = ({
   href,
   label,
   onClick,
+  className,
   isExternalLink = false,
 }: SidebarLinkProps) => {
   if (isExternalLink) {
@@ -35,7 +37,7 @@ const SidebarLink = ({
 
   return (
     <div onClick={onClick}>
-      <Link className='flex items-center gap-x-2 cursor-pointer' href={href}>
+      <Link className={'flex items-center gap-x-2 cursor-pointer ' + className} href={href}>
         <span className='text-black-6'>{label}</span>
       </Link>
     </div>
@@ -52,32 +54,82 @@ const MobileSidebar = ({ handleOnToggle, }: MobileSidebarProps) => {
     <nav
 
       className={(
-        'bg-sky-50 h-full w-full fixed top-0 left-0 z-20 p-5'
+        'bg-[#ECE0EA] h-full w-full fixed top-0 left-0 z-20 p-5'
       )}
     >
-      <div className='h-full max-w-7xl mx-auto flex flex-col'>
+      <div className='h-full max-w-7xl mx-auto flex flex-col '>
         <section className='flex justify-between pb-2 border-b border-gray-700 text-gray-700'>
-          <div>ชื่อร้านค้า</div>
-          <div className='text-gray-700 flex gap-2 items-center border border-gray-700 rounded-md px-3 py-1 cursor-pointer' onClick={handleClose}>
-          X ปิดเมนู
+          <div className="text-gray-700 font-bold">Bloem.wednesday</div>
+          <div className='text-gray-700 flex gap-2 items-center rounded-md px-3 py-1  font-boldcursor-pointer' onClick={handleClose}>
+          X 
+          {/* <span className='text-xs'>close menu</span> */}
           </div>
         </section>
 
         <section className='flex flex-col justify-between h-full'>
-          <div className='flex flex-col mt-10 gap-y-6 text-gray-700'>
+          <div className='flex flex-col mt-5 gap-y-6 text-gray-700'>
+            <div className=' pb-5  flex flex-col gap-y-5 border-b-2 border-dashed border-pink-300'>
+            <SidebarLink
+              href='/view-your-collection'
+              icon={''}
+              label={'Your collection'}
+              onClick={handleClose}
+            />
+               <SidebarLink
+              href='/customize-your-collection'
+              icon={''}
+              label={'Customize your own'}
+              onClick={handleClose}
+            />
+            </div>
+
+            <div className='pb-5  flex flex-col gap-y-5 border-b-2 border-dashed border-pink-300'>
+            <SidebarLink
+              href='/view-viva-collection'
+              icon={''}
+              label={'Viva collection'}
+              onClick={handleClose}
+            />
+               <SidebarLink
+              href='customize-viva-collection'
+              icon={''}
+              label={'Customize your own'}
+              onClick={handleClose}
+            />
+            </div>
+
+            <div className='pb-5  flex flex-col gap-y-5 border-b-2 border-dashed border-pink-300'>
             <SidebarLink
               href='/flower-a'
               icon={''}
-              label={'ดอกไม้ A'}
+              label={'Your collection'}
               onClick={handleClose}
             />
-
-<SidebarLink
-              href='/flower-b'
+               <SidebarLink
+              href='/flower-a'
               icon={''}
-              label={'ดอกไม้ B'}
+              label={'Customize your own'}
               onClick={handleClose}
             />
+            </div>
+
+            <div className=' border-gray-800 flex flex-col gap-y-5'>
+            <SidebarLink
+              href='/flower-a'
+              icon={''}
+              label={'Your collection'}
+              onClick={handleClose}
+            />
+               <SidebarLink
+              href='/flower-a'
+              icon={''}
+              label={'Customize your own'}
+              onClick={handleClose}
+            />
+            </div>
+
+            
+
           </div>
 
       
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,14 +12,14 @@ export default function Navbar() {
   return (
     <nav className="border-gray-200 bg-gray-50 cursor-pointer">
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-   <div className="text-gray-700">ชื่อร้านค้า</div>
+   <div className="text-gray-700 font-bold">Bloem.wednesday</div>
 
       <button onClick={() =>handleOnToggleMobileSidebar(true)} className="text-gray-700 flex gap-2 items-center border border-gray-700 rounded-md px-3 py-1 cursor-pointer">
      
       <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
       </svg>
-      <span>รายการดอกไม้</span>
+      <span className='text-xs'>Menu</span>
       </button>
 
 {isMobileSidebarOpen &&  <MobileSidebar handleOnToggleMobileSidebar={handleOnToggleMobileSidebar}/>}
